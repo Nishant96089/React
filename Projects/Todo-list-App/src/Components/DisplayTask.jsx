7b93e4ea80bdc5f4f7505ff3f36dcee3dc93825f
Nishant Sinha <nishantsinha.hulk@gmail.com>
2024-02-20 18:18:27 +0530

    Rendering data in AddTask by useRef now.

--- Projects/Todo-list-App/src/Components/DisplayTask.jsx
+++ Projects/Todo-list-App/src/Components/DisplayTask.jsx
@@ -2,14 +2,14 @@ import { MdOutlineDeleteForever } from "react-icons/md";
 
 function DisplayTask({ taskname, taskdate, delBtn }) {
   return (
-    <div class="container items-container">
-      <div class="row">
-        <div class="col-6">{taskname}</div>
-        <div class="col-4">{taskdate}</div>
-        <div class="col-2">
+    <div className="container items-container">
+      <div className="row">
+        <div className="col-6">{taskname}</div>
+        <div className="col-4">{taskdate}</div>
+        <div className="col-2">
           <button
             type="button"
-            class="btn btn-danger"
+            className="btn btn-danger"
             onClick={() => delBtn(taskname)}
           >
             <MdOutlineDeleteForever />
--- Projects/Todo-list-App/src/Components/InputTask.jsx
+++ Projects/Todo-list-App/src/Components/InputTask.jsx
@@ -1,20 +1,25 @@
+import { useRef } from "react";
 import { MdAddComment } from "react-icons/md";
 
 function InputTask({ addTask }) {
-  const submitBehaviour = (event) => {
+  const textRef = useRef(null);
+  const dateRef = useRef(null);
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    addTask(text.value, date.value);
-    text.value = "";
-    date.value = "";
+    addTask(textRef.current.value, dateRef.current.value);
+    textRef.current.value = "";
+    dateRef.current.value = "";
   };
+
   return (
-    <form onSubmit={submitBehaviour} className="container text-center">
+    <form onSubmit={handleSubmit} className="container text-center">
       <div className="row">
         <div className="col-6">
-          <input type="text" placeholder="Enter your task" id="text" />
+          <input type="text" placeholder="Enter your task" ref={textRef} />
         </div>
         <div className="col-4">
-          <input type="date" id="date" />
+          <input type="date" ref={dateRef} />
         </div>
         <div className="col-2">
           <button className="btn btn-success">
