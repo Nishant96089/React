b1dda58fe7fac89df399abfed8ab4a16be21dc2b
Nishant Sinha <nishantsinha.hulk@gmail.com>
2024-03-18 14:12:46 +0530

    Course progress

--- Projects/Clock/src/components/CurrentTime.jsx
+++ Projects/Clock/src/components/CurrentTime.jsx
@@ -1,11 +1,19 @@
-function CurrentTime() {
-  let time = new Date();
+import { useEffect, useState } from "react";
 
-  return (
-    <p className="fs-4">
-      Current Time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}
-    </p>
-  );
+function CurrentTime() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+
+  return <p className="fs-4">Current Time: {time.toLocaleString()}</p>;
 }
 
 export default CurrentTime;
